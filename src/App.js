96019d76241bacd487fb1014484fc5813db7d895
yuvalnir11 <yuvalnir11@gmail.com>
2021-08-29 17:18:01 +0300

    added intro animation and some more good stuff

--- src/App.js
+++ src/App.js
@@ -1,70 +1,74 @@
 import "./App.css";
 import Skills from "./components/Skills";
 import ProfilePic from "./images/ProfilePic.jpg";
-import Intro from "./components/Intro"
+import Intro from "./components/Intro";
+import Projects from "./components/Projects";
+import Farewell from "./components/Farewell";
 import { github, linkedin } from "./images/icons";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="main-container">
-        <div className="glass">
-          <div className="dashboard">
-            <div className="user">
-              <img src={ProfilePic} alt="" />
-              <h4>Yuval Nir</h4>
-              <h5>Full Stack Developer</h5>
+    return (
+        <div className="App">
+            <div className="main-container">
+                <div className="glass">
+                    <div className="dashboard">
+                        <div className="user">
+                            <img src={ProfilePic} alt="" />
+                            <h4>Yuval Nir</h4>
+                            <h5>Full Stack Developer</h5>
+                        </div>
+                        <div className="contact-info">
+                            <h3>Contact Info</h3>
+                            <div className="mobile">
+                                <p>0503902417</p>
+                            </div>
+                            <div className="email">
+                                <p>yuvalnir11@gmail.com</p>
+                            </div>
+                        </div>
+                        <div className="links">
+                            <h3>Links</h3>
+                            <div className="links-grid">
+                                <a
+                                    className="github"
+                                    href="https://github.com/yuvalnir"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    {github}
+                                </a>
+                                <a
+                                    className="linkedin"
+                                    href="https://www.linkedin.com/in/yuval-nir-92775218a/"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    {linkedin}
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="content">
+                        {/* <h2>Here comes the content</h2> */}
+                        <Intro></Intro>
+                        <Skills></Skills>
+                        <Projects></Projects>
+                        <Farewell></Farewell>
+                    </div>
+                </div>
+                <div className="circle1 circle-definition"></div>
+                <div className="circle2 circle-definition"></div>
+                <div className="circle3 circle-definition"></div>
+                <div className="circle4 circle-definition"></div>
+                <div className="circle5 circle-definition"></div>
+                <div className="circle6 circle-definition"></div>
+                <div className="circle7 circle-definition"></div>
+                <div className="circle8 circle-definition"></div>
+                <div className="circle9 circle-definition"></div>
+                <div className="circle10 circle-definition"></div>
             </div>
-            <div className="contact-info">
-              <h3>Contact Info</h3>
-              <div className="mobile">
-                <p>0503902417</p>
-              </div>
-              <div className="email">
-                <p>yuvalnir11@gmail.com</p>
-              </div>
-            </div>
-            <div className="links">
-              <h3>Links</h3>
-              <div className="links-grid">
-                <a
-                  className="github"
-                  href="https://github.com/yuvalnir"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {github}
-                </a>
-                <a
-                  className="linkedin"
-                  href="https://www.linkedin.com/in/yuval-nir-92775218a/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {linkedin}
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="content">
-            {/* <h2>Here comes the content</h2> */}
-            <Intro></Intro>
-            <Skills></Skills>
-          </div>
         </div>
-        <div className="circle1"></div>
-        <div className="circle2"></div>
-        <div className="circle3"></div>
-        <div className="circle4"></div>
-        <div className="circle5"></div>
-        <div className="circle6"></div>
-        <div className="circle7"></div>
-        <div className="circle8"></div>
-        <div className="circle9"></div>
-        <div className="circle10"></div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,12 +1,11 @@
-
 function Intro() {
-    return(
+    return (
         <div className="intro">
-            <h3>Hi,</h3>
-            <h3>My name is Yuval Nir,</h3>
-            <h3>I'm a full stack developer!</h3>
-            <h3>This site is here to show you some of my skills and work.</h3>
-            <h3>Enjoy</h3>
+            <h3 className="intro1 intro-fade">Hi,</h3>
+            <h3 className="intro2 intro-fade">My name is Yuval,</h3>
+            <h3 className="intro3 intro-fade">And i'm a full stack developer!</h3>
+            <h3 className="intro4 intro-fade">This site is here to show you some of my skills and work.</h3>
+            <h3 className="intro5 intro-fade">Enjoy</h3>
         </div>
     );
 }
