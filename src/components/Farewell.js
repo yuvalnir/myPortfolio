6c71c5fb42fea0533e3f56189ab4f8583e6d8508
yuvalnir11 <yuvalnir11@gmail.com>
2021-08-29 17:15:54 +0300

    new farewell section

---
+++ src/components/Farewell.js
@@ -0,0 +1,16 @@
+
+
+function Farewell() {
+    return (
+        <div className="farewell">
+            <h3 className="intro1 intro-fade">Thank you for viewing my site</h3>
+            <h3 className="intro2 intro-fade">I hope you found it interesting</h3>
+            <h3 className="intro3 intro-fade">For some more information about me</h3>
+            <h3 className="intro4 intro-fade">Please check my linkedIn profile</h3>
+            <div className="farewell-linkedin-div"></div>
+            <h3 className="intro5 intro-fade">And if you like what you see feel free to contact me</h3>
+        </div>
+    );
+}
+
+export default Farewell;
