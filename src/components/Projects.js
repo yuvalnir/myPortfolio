eadcd9bce850e5eddcf0f10060d4d420c1606006
yuvalnir11 <yuvalnir11@gmail.com>
2021-09-16 21:18:08 +0300

    added some adjustments

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,25 +2,35 @@ import Project from "./Project";
 
 function Projects() {
     const name1 = "The Cook'sGram";
-    const name2 = "Tenants Manager";
+    const name2 = "Chaizu - Landing page";
+    const name3 = "Tenants Manager";
     const description1 = "Recipe site, in the future will be a social network";
-    const description2 = "App to manage tenant's";
-    const link1 = "";
-    const link2 = "";
+    const description2 = "Open-source project on its first steps";
+    const description3 = "Small app for managing tenant's";
+    const link1 = "https://github.com/yuvalnir/TheCooksGram";
+    const link2 = "https://github.com/arielferdman/pm";
+    const link3 = "https://github.com/yuvalnir/tenant-manager";
 
     return (
         <div className="projects">
             <h1 className="projects-title" >Projects</h1>
-            <Project
-                name={name1}
-                description={description1}
-                link={link1}
-            ></Project>
-            <Project
-                name={name2}
-                description={description2}
-                link={link2}
-            ></Project>
+            <div className="projects-container">
+                <Project
+                    name={name1}
+                    description={description1}
+                    link={link1}
+                ></Project>
+                <Project
+                    name={name2}
+                    description={description2}
+                    link={link2}
+                ></Project>
+                <Project
+                    name={name3}
+                    description={description3}
+                    link={link3}
+                ></Project>
+            </div>
         </div>
     );
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,13 +2,18 @@ import { vue, react, html, css, js, node } from "../images/icons";
 
 function Skills() {
     return (
-        <div className="skill">
-            <div className="skill-icon">{vue}</div>
-            <div className="skill-icon">{react}</div>
-            <div className="skill-icon">{js}</div>
-            <div className="skill-icon">{html}</div>
-            <div className="skill-icon">{css}</div>
-            <div className="skill-icon">{node}</div>
+        <div className="skills">
+            <div className="skills-title">
+                <h1>Skills</h1>
+            </div>
+            <div className="skill-grid">
+                <div className="skill-icon">{vue}</div>
+                <div className="skill-icon">{react}</div>
+                <div className="skill-icon">{js}</div>
+                <div className="skill-icon">{html}</div>
+                <div className="skill-icon">{css}</div>
+                <div className="skill-icon">{node}</div>
+            </div>
         </div>
     );
 }
